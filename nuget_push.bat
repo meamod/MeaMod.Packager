d61d2ebb692b6790370d7901a7fd71dbdf16ab20
James Weston <james@fe0.uk>
2022-12-29 15:55:24 +0000

    Release 1.1.0

---
+++ nuget_push.bat
@@ -0,0 +1,20 @@
+set mypath=%cd%
+echo %mypath%
+
+set SEARCH_BASE="MeaMod.Packager\bin\Release"
+set FILTER=*.DLL
+for /r %SEARCH_BASE% %%i in (%FILTER%) do (
+signtool verify /pa /q "%%i" 1>nul 2>nul 
+if errorlevel 1 (
+signtool sign /n "James Weston" /fd SHA256 /d "MeaMod.Packager" /du "https://www.meamod.com" /tr http://timestamp.digicert.com /td sha256 "%%i"
+)
+)
+
+dotnet pack -c Release --no-build
+
+nuget sign "MeaMod.Packager\bin\Release\*.nupkg" -CertificateSubjectName "James Weston" -Timestamper http://timestamp.digicert.com -NonInteractive
+
+nuget push "MeaMod.Packager\bin\Release\*.nupkg" -Source nuget.org -SkipDuplicate -NonInteractive
+
+move "MeaMod.Packager\bin\Release\*.nupkg" nugetpackagearchive
+
